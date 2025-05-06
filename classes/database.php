f4223fd608194463836acf82decfdadb14deda6e
KielMartinHernandezMano <manokh@students.nu-lipa.edu.ph>
2025-05-06 16:56:53 +0800

    esrfseAW

---
+++ classes/database.php
@@ -0,0 +1,33 @@
+<?php
+ 
+class database {
+ 
+    function opencon(): PDO {
+        $pdo = new PDO(
+            dsn:'mysql:host=localhost;dbname=dbs_db',
+            username: 'root',
+            password: ''
+        );
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    }
+ 
+    function signupUser($firstname, $lastname, $username, $password) {
+        $con = $this->opencon();
+ 
+        try {
+            $con->beginTransaction();
+            $stmt = $con->prepare("INSERT INTO admin (admin_FN, admin_LN, admin_username, admin_password) VALUES (?, ?, ?, ?)");
+            $stmt->execute([$firstname, $lastname, $username, $password]);
+            $userId = $con->lastInsertId();
+            $con->commit();
+            return $userId;
+        } catch (PDOException $e) {
+            $con->rollBack();
+            return false;
+        }
+    }
+}
+
+  git config --global user.email "manokh@students.nu-lipa.edu.ph"
+  git config --global user.name "KielMartinHernandezMano"
